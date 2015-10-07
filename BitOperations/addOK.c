2e76595a6dfbde61f7ecdd0dc1fa5a997bcf977e
Incaze <e.kalabkin@gmail.com>
2015-10-07 16:47:36 +0300

    Create addOK.c

---
+++ BitOperations/addOK.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+int addOK(int x, int y)
+ {
+	return (!(~((x>>31)^(y>>31))&((x>>31)^((x+y)>>31))));
+ }
+ int main()
+{
+  int a1,a2;
+  scanf("%x%x",&a1,&a2);
+  printf("%d\n",addOK(a1,a2));
+}
