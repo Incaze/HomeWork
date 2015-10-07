0260aa3e994834e0b9b1a507e4cd45510f5774e2
Incaze <e.kalabkin@gmail.com>
2015-10-07 16:56:33 +0300

    Create fitsBits.c

---
+++ BitOperations/fitsBits.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+int fitsBits(int x, int n)
+ {
+    return (!(((~x & (x>>31)) + (x & ~(x>>31))) >> (n + ~0)));
+ }
+ int main()
+{
+  int a1,a2;
+  scanf("%d%d",&a1,&a2);
+  printf("%d\n",fitsBits(a1,a2));
+}
