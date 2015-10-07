70d606430d69e658431efcbbd5a4ed2d174d86f0
Incaze <e.kalabkin@gmail.com>
2015-10-07 16:58:09 +0300

    Create getByte.c

---
+++ BitOperations/getByte.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+int getByte(int x, int n)
+ {
+     return((x>>(n<<3))&255);
+ }
+ int main()
+{
+  int a1,a2;
+  scanf("%x%d",&a1,&a2);
+  printf("0x%x\n",getByte(a1,a2));
+}
