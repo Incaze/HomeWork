b7e0f60b247e348d24cef5642780a2e21aec0cd7
Incaze <e.kalabkin@gmail.com>
2015-10-15 17:16:48 +0300

    Update bitAnd.c

--- BitOperations/bitAnd.c
+++ BitOperations/bitAnd.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <math.h>
+
 int bitAnd(int x, int y)
- {
-	return ~(~x | ~y);
- }
- int main()
 {
-  int a1,a2;
-  scanf("%d%d",&a1,&a2);
-  printf("%d\n",bitAnd(a1,a2));
+    return ~(~x | ~y);
+}
+
+int main()
+{
+    int a1, a2;
+    scanf("%d%d",&a1,&a2);
+    printf("%d\n",bitAnd(a1,a2));
 }
