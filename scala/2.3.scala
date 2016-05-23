1c2221f5d49e424ed598cbb710dc8b06ec8c5bd1
Incaze <e.kalabkin@gmail.com>
2016-05-23 14:25:53 +0300

    Create 2.3.scala

---
+++ scala/2.3.scala
@@ -0,0 +1,13 @@
+object solve {
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if ((coins.isEmpty) || (money < 0)) {
+      0
+    }
+    else if (money == 0) {
+      1
+    }
+    else {
+      countChange(money, coins.tail) + countChange(money - coins.head, coins)
+    }
+  }
+}
